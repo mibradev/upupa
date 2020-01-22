41697874c1f27f52f0b5f5a369da845ebd811836
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2020-01-22 02:32:02 +0200

    Style home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,6 +6,5 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
     assert_response :ok
-    assert_select "h1", "Upupa"
   end
 end
---
+++ test/system/home_page_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class HomePageTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit root_url
+    assert_selector "h1", text: "U P U P A"
+  end
+end
