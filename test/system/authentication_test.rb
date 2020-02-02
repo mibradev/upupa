b1bfc813c57fbdcad1d8beaaf823e291501bad38
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2020-02-02 12:32:36 +0200

    Use "Log in" instead of "Login"

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -12,17 +12,17 @@ class AuthenticationTest < ApplicationSystemTestCase
     visit new_user_session_url
     assert_page_title "Login"
     assert_page_description "Log into Upupa to access your account."
-    assert_selector "h2", text: "Login"
+    assert_selector "h2", text: "Log in"
     fill_in "Email", with: @user.email
     fill_in "Password", with: @password
-    click_button "Login"
+    click_button "Log in"
     assert_equal root_path, current_path
     assert_text I18n.t("devise.sessions.signed_in")
   end
 
   test "failed login" do
     visit new_user_session_url
-    click_button "Login"
+    click_button "Log in"
     assert_equal new_user_session_path, current_path
     assert_text I18n.t("devise.failure.invalid", authentication_keys: "Email")
   end
@@ -30,14 +30,14 @@ class AuthenticationTest < ApplicationSystemTestCase
   test "invalid email" do
     visit new_user_session_url
     fill_in "Password", with: @password
-    click_button "Login"
+    click_button "Log in"
     assert_text I18n.t("devise.failure.invalid", authentication_keys: "Email")
   end
 
   test "invalid password" do
     visit new_user_session_url
     fill_in "Email", with: @user.email
-    click_button "Login"
+    click_button "Log in"
     assert_text I18n.t("devise.failure.invalid", authentication_keys: "Email")
   end
 end
