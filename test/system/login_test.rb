ba2ae68cc09d2dc792339c8b671d24d36f1e8c6d
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2020-02-13 20:31:28 +0200

    Add some regression tests

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -5,15 +5,9 @@ require "application_system_test_case"
 class LoginTest < ApplicationSystemTestCase
   test "logging in" do
     visit new_user_session_url
-    assert_page_title "Login"
-    assert_page_description "Log into Upupa to access your account."
-    assert_selector "h2", text: "Log in"
-    assert_selector "input[type='email'][required]"
-    assert_selector "input[type='password'][required]"
     fill_in "Email", with: users(:one).email
     fill_in "Password", with: "12345678"
     click_button "Log in"
-    assert_equal root_path, current_path
     assert_text I18n.t("devise.sessions.signed_in")
   end
 
