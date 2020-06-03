0c57e89028e35ec922cf055939fa0dd6cb1883f6
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2020-06-03 13:41:04 +0200

    Don't import everything from Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,15 @@ require("@rails/activestorage").start()
 import "channels"
 import "controllers"
 
-import $ from "jquery"
-import "bootstrap"
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="popover"]').popover()
-  $('[data-toggle="tooltip"]').tooltip()
-})
+import "bootstrap/js/dist/alert"
+// import "bootstrap/js/dist/button"
+// import "bootstrap/js/dist/carousel"
+// import "bootstrap/js/dist/collapse"
+// import "bootstrap/js/dist/dropdown"
+// import "bootstrap/js/dist/modal"
+// import "bootstrap/js/dist/popover"
+// import "bootstrap/js/dist/scrollspy"
+// import "bootstrap/js/dist/tab"
+// import "bootstrap/js/dist/toast"
+// import "bootstrap/js/dist/tooltip"
+// import "bootstrap/js/dist/util"
