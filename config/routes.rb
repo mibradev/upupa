1b84707d1c7c61d36db44f6c4b5cfbe10fb98f72
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2020-12-25 16:35:56 +0200

    Update rails files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   devise_scope :user do
     get "login", to: "devise/sessions#new", as: "new_user_session"
     post "login", to: "devise/sessions#create", as: "user_session"
