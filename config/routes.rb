c40afe7abbf52d4b502aab12b91ea36f9f666ff7
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-15 19:06:49 +0200

    Improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 
-  resources :work_types
+  devise_for :users, skip: :all
 
   devise_scope :user do
     get "login", to: "devise/sessions#new", as: "new_user_session"
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
     delete "logout", to: "devise/sessions#destroy", as: "destroy_user_session"
   end
 
-  devise_for :users, skip: :sessions
-  root to: "home#index"
+  resources :work_types
 end
