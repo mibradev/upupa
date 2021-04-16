4a11f7c58a14263b4167fc8bc0e666995bab3301
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-16 08:31:13 +0200

    Refactor Role#name

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,7 +5,7 @@ class Role < ApplicationRecord
   has_and_belongs_to_many :users
 
   def name=(value)
-    value = value.squish unless value.nil?
+    value.squish! unless value.nil?
     super(value)
   end
 end
