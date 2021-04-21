72ad7fcaec914fc539f92eaecdc0fc9ab4a0097f
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-21 22:57:38 +0200

    Use generic name for resources

--- app/controllers/work_types_controller.rb
+++ app/controllers/work_types_controller.rb
@@ -2,46 +2,46 @@ class WorkTypesController < ApplicationController
   before_action :set_work_type, only: [:show, :edit, :update, :destroy]
 
   def index
-    @work_types = WorkType.all
+    @resources = WorkType.all
   end
 
   def show
   end
 
   def new
-    @work_type = WorkType.new
+    @resource = WorkType.new
   end
 
   def edit
   end
 
   def create
-    @work_type = WorkType.new(work_type_params)
+    @resource = WorkType.new(work_type_params)
 
-    if @work_type.save
-      redirect_to(@work_type, notice: "Work type was successfully created.")
+    if @resource.save
+      redirect_to(@resource, notice: "Work type was successfully created.")
     else
       render :new
     end
   end
 
   def update
-    if @work_type.update(work_type_params)
-      redirect_to @work_type, notice: "Work type was successfully updated."
+    if @resource.update(work_type_params)
+      redirect_to @resource, notice: "Work type was successfully updated."
     else
       render :edit
     end
   end
 
   def destroy
-    @work_type.destroy
+    @resource.destroy
     redirect_to work_types_url, notice: "Work type was successfully destroyed."
   end
 
   private
 
   def set_work_type
-    @work_type = WorkType.find(params[:id])
+    @resource = WorkType.find(params[:id])
   end
 
   def work_type_params
