abe5ea288deff5ec004ad1ac9f72eb4a3733abd1
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-22 20:28:48 +0200

    Renove devise layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, skip: :all
 
   devise_scope :user do
-    get "login", to: "users/sessions#new", as: "new_user_session"
-    post "login", to: "users/sessions#create", as: "user_session"
-    delete "logout", to: "users/sessions#destroy", as: "destroy_user_session"
+    get "login", to: "devise/sessions#new", as: "new_user_session"
+    post "login", to: "devise/sessions#create", as: "user_session"
+    delete "logout", to: "devise/sessions#destroy", as: "destroy_user_session"
   end
 
   resources :work_types
