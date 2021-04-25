2a51653bda13443ca2a4fc4d57543b6ffe8d414d
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-26 00:33:29 +0200

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './app/helpers/**/*.rb',
+    './app/views/**/*.html.erb'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
