1663f23eeff312f77ed474de4cb1c4ec8dc7c083
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-26 01:03:58 +0200

    Don't care about the style in tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,6 +6,6 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
     assert_page_title I18n.t(:app_name)
     assert_page_description "#{I18n.t :app_name} is a web application that is trying to make life easier for translators."
-    assert_select "h1", I18n.t(:app_name).upcase
+    assert_select "h1", I18n.t(:app_name)
   end
 end
