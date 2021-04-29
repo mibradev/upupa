3a8e06d794254145e66ce6bb18ec5afbfee38fdb
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-04-29 23:11:08 +0200

    Remove scaffold controller

--- app/controllers/word_count_files_controller.rb
+++ app/controllers/word_count_files_controller.rb
@@ -1,2 +1,76 @@
-class WordCountFilesController < ScaffoldController
+class WordCountFilesController < ApplicationController
+  include Pundit
+  layout "scaffold"
+
+  before_action :set_resource, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @resources = resource_model.all
+    render "scaffold/index"
+  end
+
+  def show
+    render "scaffold/show"
+  end
+
+  def new
+    @resource = resource_model.new
+    render "scaffold/new"
+  end
+
+  def edit
+    render "scaffold/edit"
+  end
+
+  def create
+    @resource = resource_model.new
+    @resource.assign_attributes resource_params
+    @resource.assign_attributes resource_attributes
+
+    if @resource.save
+      redirect_to @resource, notice: resource_notice
+    else
+      render "scaffold/new"
+    end
+  end
+
+  def update
+    if @resource.update(resource_params)
+      redirect_to @resource, notice: resource_notice
+    else
+      render "scaffold/edit"
+    end
+  end
+
+  def destroy
+    if @resource.destroy
+      flash.notice = resource_notice
+    else
+      flash.alert = @resource.errors.full_messages.first
+    end
+
+    redirect_to action: :index
+  end
+
+  private
+
+  def set_resource
+    @resource = resource_model.find(params[:id])
+  end
+
+  def resource_params
+    permitted_attributes @resource
+  end
+
+  def resource_model
+    controller_name.classify.constantize
+  end
+
+  def resource_attributes
+    {}
+  end
+
+  def resource_notice
+    I18n.t("scaffold.#{action_name}.notice", resource: resource_model.model_name.human)
+  end
 end
