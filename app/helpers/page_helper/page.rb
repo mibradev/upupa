1ea6590e2be2931b043af41a7ff70d0f8005a344
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-05-17 07:13:45 +0200

    Replace partials with helpers for description list

--- app/helpers/page_helper/page.rb
+++ app/helpers/page_helper/page.rb
@@ -6,6 +6,12 @@ module PageHelper
       end
     end
 
+    def description_list
+      @template.tag.dl(class: "text-gray-600 bg-orange-50 rounded divide-y divide-gray-300 shadow") do
+        yield DescriptionList.new(@template)
+      end
+    end
+
     def section
       @template.tag.section(class: "space-y-8") do
         yield Section.new(@template)
