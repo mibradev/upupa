7ecffb729812cc8fe99ede7a4be5b95aa2a2aead
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-05-21 09:19:58 +0200

    Remove unused test helpers

--- test/test_helpers/system_test_assertions.rb
+++ test/test_helpers/system_test_assertions.rb
@@ -1,12 +1,3 @@
 class ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
-
-  def assert_page_title(title)
-    assert page.has_title?(title)
-  end
-
-  def assert_page_description(content)
-    element = page.find("meta[name='description']", visible: false)
-    assert_equal content, element[:content]
-  end
 end
