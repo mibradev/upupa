730cc2192d9567a6f205b9b91de4c77e69972e58
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-08-24 10:02:03 +0200

    Update form styles

--- app/helpers/application_helper/page/form.rb
+++ app/helpers/application_helper/page/form.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
         options[:builder] = FormBuilder
 
         @template.form_with(model: model, scope: scope, url: url, format: format, **options) do |form|
-          @template.tag.div(class: "space-y-4 sm:p-8 sm:text-gray-600 sm:bg-orange-50 sm:rounded sm:shadow") do
+          @template.tag.div(class: "space-y-8 sm:p-8 sm:text-gray-600 sm:bg-orange-50 sm:rounded sm:shadow") do
             @template.concat form.errors
             @template.concat @template.capture(form, &block)
           end
@@ -16,7 +16,7 @@ module ApplicationHelper
         def errors
           return unless object&.errors&.any?
 
-          @template.tag.div(class: "p-4 bg-red-900 rounded") do
+          @template.tag.div(class: "p-8 bg-red-900 rounded") do
             @template.tag.h4(class: "mb-2 font-medium text-red-300") do
               "#{@template.pluralize(object.errors.count, "error")} prohibited this record from being saved:"
             end +
@@ -29,37 +29,37 @@ module ApplicationHelper
         end
 
         def label(method, text = nil, options = {}, &block)
-          options[:class] = "block font-medium"
+          options[:class] = "block font-medium appearance-none"
           super
         end
 
         def collection_select(method, collection, value_method, text_method, options = {}, html_options = {})
-          html_options[:class] = "block p-2 w-full bg-white rounded border focus:border-0 focus:shadow focus:outline-none"
+          html_options[:class] = "block px-3 py-2 w-full bg-white rounded border appearance-none focus:border-0 focus:shadow focus:outline-none"
           @template.tag.div(class: "space-y-2") { label(method) + super }
         end
 
         def date_field(method, options = {})
-          options[:class] = "block py-2 px-3 w-full rounded border focus:border-0 focus:shadow focus:outline-none"
+          options[:class] = "block px-3 py-2 w-full bg-white rounded border appearance-none focus:border-0 focus:shadow focus:outline-none"
           @template.tag.div(class: "space-y-2") { label(method) + super }
         end
 
         def number_field(method, options = {})
-          options[:class] = "block py-2 px-3 w-full rounded border focus:border-0 focus:shadow focus:outline-none"
+          options[:class] = "block px-3 py-2 w-full bg-white rounded border appearance-none focus:border-0 focus:shadow focus:outline-none"
           @template.tag.div(class: "space-y-2") { label(method) + super }
         end
 
         def text_area(method, options = {})
-          options[:class] = "block py-2 px-3 w-full rounded border focus:border-0 focus:shadow focus:outline-none"
+          options[:class] = "block px-3 py-2 w-full bg-white rounded border appearance-none focus:border-0 focus:shadow focus:outline-none"
           @template.tag.div(class: "space-y-2") { label(method) + super }
         end
 
         def text_field(method, options = {})
-          options[:class] = "block py-2 px-3 w-full rounded border focus:border-0 focus:shadow focus:outline-none"
+          options[:class] = "block px-3 py-2 w-full bg-white rounded border appearance-none focus:border-0 focus:shadow focus:outline-none"
           @template.tag.div(class: "space-y-2") { label(method) + super }
         end
 
         def submit(value = nil, options = {})
-          options[:class] = "block py-2 px-4 text-orange-50 bg-gray-600 rounded shadow cursor-pointer hover:bg-gray-700 focus:ring-2 focus:ring-gray-700 focus:ring-opacity-50 focus:outline-none"
+          options[:class] = "block py-2 px-4 text-orange-50 bg-gray-600 rounded shadow appearance-none cursor-pointer hover:bg-gray-700 focus:ring-2 focus:ring-gray-700 focus:ring-opacity-50 focus:outline-none"
           super
         end
       end
