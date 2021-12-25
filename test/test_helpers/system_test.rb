9f2d2ca9d5d6e59f6630c8a700c48acfca605395
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-12-25 18:23:52 +0200

    Localize session actions

--- test/test_helpers/system_test.rb
+++ test/test_helpers/system_test.rb
@@ -1,9 +1,9 @@
 class ActionDispatch::SystemTestCase
   def log_in(user, password: "12345678")
     visit login_url
-    fill_in "Email", with: user.email
-    fill_in "Password", with: password
-    click_button "Log in"
+    fill_in User.human_attribute_name(:email), with: user.email
+    fill_in User.human_attribute_name(:password), with: password
+    click_button I18n.t("actions.sessions.log_in")
     has_text? I18n.t("notices.sessions.logged_in")
   end
 end
