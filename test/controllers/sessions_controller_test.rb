e38e2ba444fe4b4b63339e4614e92cd1ed18380d
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-12-27 15:48:06 +0200

    Don't use log_in helper in session tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -12,7 +12,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not get login if already logged in" do
-    log_in @user
+    post login_url, params: {email: @user.email, password: @password}
     get login_url
     assert_equal I18n.t("alerts.sessions.already_logged_in"), flash.alert
     assert_redirected_to root_url
@@ -26,8 +26,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not log in if already logged in" do
-    log_in @user
-    post login_url, params: {email: @user.email, password: @password}
+    2.times { post login_url, params: {email: @user.email, password: @password} }
     assert_equal I18n.t("alerts.sessions.already_logged_in"), flash.alert
     assert_redirected_to root_url
   end
@@ -57,7 +56,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should log out" do
-    log_in @user
+    post login_url, params: {email: @user.email, password: @password}
     delete logout_url
     assert_nil session[:user_id]
     assert_equal I18n.t("notices.sessions.logged_out"), flash.notice
@@ -71,7 +70,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not cause too many redirects after destroying logged in user" do
-    log_in @user
+    post login_url, params: {email: @user.email, password: @password}
     @user.destroy!
     get login_url
     assert_response :ok
