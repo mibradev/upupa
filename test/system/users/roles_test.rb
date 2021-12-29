63d3d448c0bd078494204eacdc835df3a8f35a4f
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2021-12-29 16:54:51 +0200

    Add system tests for user roles

---
+++ test/system/users/roles_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class Users::RolesTest < ApplicationSystemTestCase
+  setup do
+    log_in users(:admin)
+    @user = users(:for_user_roles)
+  end
+
+  test "creating a user role" do
+    visit user_url(@user)
+
+    within("#user_roles") do
+      select roles(:one).name, from: UserRole.human_attribute_name(:role)
+      click_on I18n.t("helpers.submit.submit")
+    end
+
+    assert_text I18n.t("notices.created", record: UserRole.model_name.human)
+  end
+
+  test "destroying a user role" do
+    visit user_url(@user)
+    within("#user_roles") { click_on I18n.t("page.actions.destroy") }
+    assert_text I18n.t("notices.destroyed", record: UserRole.model_name.human)
+  end
+end
