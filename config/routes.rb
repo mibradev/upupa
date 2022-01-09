61013323de244b1de97f75c1251d998a715512e5
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2022-01-09 21:59:30 +0200

    Wrap session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  controller :sessions do
+    get "login", action: :new
+    post "login", action: :create
+    delete "logout", action: :destroy
+  end
 
   resources :roles
 
