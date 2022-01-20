b90d6997255688bccd82b48ab5f72651443fe736
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2022-01-20 11:19:44 +0200

    Remove permissions

--- app/models/permission.rb
+++
@@ -1,8 +0,0 @@
-class Permission < ApplicationRecord
-  validates :name, presence: true
-  validates :name, uniqueness: {case_sensitive: false}
-
-  def name=(value)
-    super value&.squish!
-  end
-end
