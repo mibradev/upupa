3095b3c3e5cdc6dc2c3d1cd2fe66181011aa0c27
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2022-01-31 00:38:03 +0200

    Don't use shorthand hash syntax

--- test/test_helpers/integration_test.rb
+++ test/test_helpers/integration_test.rb
@@ -1,5 +1,5 @@
 class ActionDispatch::IntegrationTest
   def log_in(user, password: "12345678")
-    post login_url, params: {email: user.email, password:}
+    post login_url, params: {email: user.email, password: password}
   end
 end
