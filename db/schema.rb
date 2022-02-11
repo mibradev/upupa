81301451c1b82f73c08e4ddccddc8a52fd7f970b
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2022-02-11 09:39:13 +0200

    Regenerate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2020_05_11_040540) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_05_11_040540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "roles", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
   create_table "user_roles", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "role_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["role_id"], name: "index_user_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_user_roles_on_user_id_and_role_id", unique: true
     t.index ["user_id"], name: "index_user_roles_on_user_id"
@@ -35,8 +34,8 @@ ActiveRecord::Schema[6.1].define(version: 2020_05_11_040540) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
@@ -48,8 +47,8 @@ ActiveRecord::Schema[6.1].define(version: 2020_05_11_040540) do
     t.bigint "word_count_id", null: false
     t.bigint "work_file_id", null: false
     t.bigint "work_type_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["word_count_id"], name: "index_word_count_files_on_word_count_id"
     t.index ["work_file_id", "work_type_id", "word_count_id"], name: "index_word_count_files_on_work_file_&_work_type_&_word_count", unique: true
     t.index ["work_file_id"], name: "index_word_count_files_on_work_file_id"
@@ -60,24 +59,24 @@ ActiveRecord::Schema[6.1].define(version: 2020_05_11_040540) do
     t.date "date", null: false
     t.text "notes"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["date", "user_id"], name: "index_word_counts_on_date_and_user_id", unique: true
     t.index ["user_id"], name: "index_word_counts_on_user_id"
   end
 
   create_table "work_files", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_work_files_on_name", unique: true
   end
 
   create_table "work_types", force: :cascade do |t|
     t.string "name", null: false
     t.decimal "multiplicand", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_work_types_on_name", unique: true
   end
 
